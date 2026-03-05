fd363779e71c9a75fac78b3df28acee425d29053
Do Ngoc Cuong <cuong01656499081@gmail.com>
2026-03-06 00:29:50 +0700

    fixxxxxx

--- Backend/test_golden_owl/src/main/java/com/example/test_golden_owl/config/SecurityConfig.java
+++ Backend/test_golden_owl/src/main/java/com/example/test_golden_owl/config/SecurityConfig.java
@@ -6,20 +6,32 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
+
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public CorsFilter corsFilter() {
+
         CorsConfiguration corsConfiguration = new CorsConfiguration();
 
-        corsConfiguration.addAllowedOriginPattern("*"); // cho phép mọi domain
-        corsConfiguration.addAllowedMethod("*");
-        corsConfiguration.addAllowedHeader("*");
-        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(Arrays.asList(
+                "https://test-go-dqup-cuongdn160624-5941s-projects.vercel.app"
+        ));
+
+        corsConfiguration.setAllowedMethods(Arrays.asList(
+                "GET", "POST", "PUT", "DELETE", "OPTIONS"
+        ));
+
+
+        corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+
+        corsConfiguration.setAllowCredentials(false);
 
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
+
         source.registerCorsConfiguration("/**", corsConfiguration);
 
         return new CorsFilter(source);
